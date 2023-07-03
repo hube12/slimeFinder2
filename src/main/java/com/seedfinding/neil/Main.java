a2fccd76a4b29d92f840487fa0f0e92f0545abca
Neil <neilhube12@gmail.com>
2023-07-04 01:39:32 +0200

    Change to account for 0,-1

--- src/main/java/com/seedfinding/neil/Main.java
+++ src/main/java/com/seedfinding/neil/Main.java
@@ -3,6 +3,7 @@ package neil;
 
 import com.seedfinding.mccore.rand.ChunkRand;
 import com.seedfinding.mccore.rand.seed.WorldSeed;
+import com.seedfinding.mccore.util.data.SpiralIterator;
 import com.seedfinding.mccore.util.pos.CPos;
 import com.seedfinding.mccore.version.MCVersion;
 import com.seedfinding.mcfeature.misc.SlimeChunk;
@@ -89,7 +90,7 @@ public class Main {
 	}
 
 	public static LongStream findSlimeSeeds2(Requirements requirements, Requirements requirements2) {
-		int bound = 17 + 4;
+		int bound = 17 + 6;
 		LongStream lowerBitsStream = LongStream.range(0, 1L << bound)
 				.filter(s -> requirements.fastCheck(s, new ChunkRand()))
 				.parallel();
@@ -130,18 +131,50 @@ public class Main {
 		MCVersion version = MCVersion.v1_16;
 		int size = 10;
 		List<CPos> positive = new ArrayList<>();
-		List<CPos> negative = generateSquare(new CPos(-size / 2, -size / 2), new CPos(size / 2, size / 2));
+		List<CPos> negative = generateSquare(new CPos(-size / 2, -size / 2).subtract(1,1), new CPos(size / 2, size / 2).subtract(1,1));
 		negative.removeIf(positive::contains);
 
 		Requirements requirements = new Requirements(positive, negative, version);
 
-		size = 11;
+		size = 10;
 		List<CPos> positive2 = new ArrayList<>();
-		List<CPos> negative2 = generateSquare(new CPos(-size / 2, -size / 2), new CPos(size / 2, size / 2));
+		List<CPos> negative2 = generateSquare(new CPos(-size / 2, -size / 2 ).subtract(2,3), new CPos(size / 2 , size / 2 ).subtract(1,1));
 		negative2.removeIf(positive2::contains);
 
 		Requirements requirements2 = new Requirements(positive2, negative2, version);
-		findSlimeSeeds2(requirements, requirements2).parallel().forEach(System.out::println);
+		findSlimeSeeds2(requirements, requirements2).parallel().forEach(s ->
+
+		{
+			int counter = 0;
+			for (int x = -3; x <= 3; x++) {
+				for (int z = -3; z <= 3; z++) {
+					int radius = slimeLessDistanceTo(s, new CPos(x, z), version);
+					if (radius > 6) {
+						counter += 1;
+					}
+				}
+			}
+			System.out.printf("%d\n", s, counter);
+			if (counter >= 5) {
+				System.out.printf("%d\n", s, counter);
+			}
+		});
+
+
+	}
+
+
+	public static int slimeLessDistanceTo(long seed, CPos pos, MCVersion version) {
+		ChunkRand rand = new ChunkRand();
+		for (int radius = 0; radius < 20; radius++) {
+			for (int x = -radius + pos.getX(); x <= radius + pos.getX(); x++) {
+				for (int z = -radius + pos.getZ(); z < radius + pos.getZ(); z++) {
+					rand.setSlimeSeed(seed, x, z, version);
+					if (rand.nextInt(10) == 0) return radius;
+				}
+			}
+		}
+		return Integer.MAX_VALUE;
 	}
 
 	public static void main(String[] args) {
